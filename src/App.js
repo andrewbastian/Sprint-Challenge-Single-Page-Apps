3801f171226d948b0699f341dffc8f0b7b9b1299
andrewbastian <andrew.bastian@gmail.com>
2019-09-13 17:40:34 -0700

    Charicter cards and 1st axios request

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import {Route} from 'react-router-dom'
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import WelcomePage from "./components/WelcomePage"
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList"
+import LocationsList from './components/LocationsList';
+//import EpisodeList from './components/EpisodeList';
+//import SearchForm from './components/SearchForm';
 
 export default function App() {
   return (
@@ -11,7 +15,8 @@ export default function App() {
 
       {/* <TabNav /> */}
       <Route exact path='/' component={WelcomePage}/>
-      <Route exact path='/' component={WelcomePage}/>
+      <Route exact path='/character' component={CharacterList}/>
     </main>
   );
 }
+ 
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
-
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [characters, setCharacters] =useState([]);
+  const [page, setPage] = useState(1)
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get(`https://rick-api.herokuapp.com/api/character/?page=${page}`)
+          .then(res =>{
+            console.log(res.data.info.page);
+            setCharacters(res.data.results);
+          })
+          .catch(error => console.log(error))
+  }, [page]);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>{characters.map(character =>{
+          return <CharacterCard key={character.id} character={character}/>
+        })}
+      </h2>
     </section>
   );
 }
